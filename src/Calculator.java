bee577a64a33f655cf46b02359fc68ea64fecb5b
Mikhail Mishin <you@example.com>
2024-04-05 17:26:13 +0300

    Added my project

---
+++ src/Calculator.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class Calculator {
+    private static boolean isValidInput(String exp) {
+        exp = exp.replaceAll("\\s", "");  // Удаляем пробелы из ввода
+        return exp.matches("^[\\d+\\-*/IVXLCDM]*$");
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] actions = {"+", "-", "/", "*"};
+            String[] regexActions = {"\\+", "-", "/", "\\*"};
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Input:");
+            String exp = scanner.nextLine();
+
+
+            // Проверяем, является ли строка математической операцией
+            if (!isValidInput(exp)) {
+                throw new IllegalArgumentException("Ошибка: строка не является математической операцией");
+            }
+
+            // Проверяем количество операторов в выражении
+            int operatorCount = 0;
+            for (int i = 0; i < actions.length; i++) {
+                if (exp.contains(actions[i])) {
+                    operatorCount++;
+                }
+            }
+            if (operatorCount != 1) {
+                throw new IllegalArgumentException("Ошибка: формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
+
+            // Находим индекс оператора
+            int actionIndex = -1;
+            for (int i = 0; i < actions.length; i++) {
+                if (exp.contains(actions[i])) {
+                    actionIndex = i;
+                    break;
+                }
+            }
+
+            // Разделяем выражение на операнды
+            String[] data = exp.split(regexActions[actionIndex]);
+            int num1, num2;
+            String data0 = data[0].replaceAll("\\s+", "");
+            String data1 = data[1].replaceAll("\\s+", "");
+
+            boolean a = RomanNumeral.isRoman(data0);
+            boolean b = RomanNumeral.isRoman(data1);
+
+            // Проверяем, используются ли одновременно разные системы счисления
+            if (a != b) {
+                throw new IllegalArgumentException("Ошибка: используются одновременно разные системы счисления");
+            }
+
+            // Преобразуем операнды в числа
+            if (a) {
+                num1 = RomanNumeral.romanToArabic(data0);
+                num2 = RomanNumeral.romanToArabic(data1);
+            } else {
+                num1 = Integer.parseInt(data0);
+                num2 = Integer.parseInt(data1);
+            }
+
+            // Проверяем диапазон чисел
+            if ((num1 > 10) || (num1 < 1) || (num2 > 10) || (num2 < 1)) {
+                throw new IllegalArgumentException("Ошибка: Должны использоваться числа от 1 до 10");
+            }
+
+            // Выполняем операцию
+            int result = switch (actions[actionIndex]) {
+                case "+" -> num1 + num2;
+                case "-" -> num1 - num2;
+                case "*" -> num1 * num2;
+                default -> num1 / num2;
+            };
+
+            // Проверяем результат на отрицательное значение
+            if (a && result <= 0) {
+                throw new IllegalArgumentException("Ошибка: в римской системе нет отрицательных чисел");
+            }
+
+            // Выводим результат
+            System.out.println("Output:");
+            if (a) {
+                String result1 = RomanNumeral.arabicToRoman(result);
+                System.out.println(result1);
+            } else {
+                System.out.println(result);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Output:");
+            System.out.println(e.getMessage());
+        }
+    }
+}
